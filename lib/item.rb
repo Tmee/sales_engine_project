3496358347bb1d612a541e111e71860d24e97217
Timothy Mee <tjmee90@gmail.com>
2014-10-12 13:56:31 -0600

    item date parsed

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,5 @@
 require 'bigdecimal'
+require 'date'
 
 class Item
     attr_reader :id,
@@ -16,13 +17,13 @@ class Item
     @description = data[:description]
     @unit_price  = BigDecimal.new(data[:unit_price])/BigDecimal(100)
     @merchant_id = data[:merchant_id].to_i
-    @created_at  = data[:created_at]
-    @updated_at  = data[:updated_at]
+    @created_at  = Date.parse(data[:created_at])
+    @updated_at  = Date.parse(data[:updated_at])
     @repository  = repository
   end
 
   def invoice_items
-    repository.find_invoice_items_by_id(id)
+    repository.find_invoice_items_by_id()
   end
 
   def merchant
